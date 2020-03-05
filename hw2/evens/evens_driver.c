a69088a5269897f45cc1683b3940340c42ea2b60
Sarah Joyce <sjoyce@ubuntu-1804.udel.edu>
2020-03-05 16:12:45 -0500

    problem 2 finished

--- hw2/evens/evens_driver.c
+++ hw2/evens/evens_driver.c
@@ -1,10 +1,29 @@
 /* Driver for evens libary */
+#include <stdio.h>
+#include <stdlib.h>
 #include "evens_lib.h"
 // add other #includes as needed
-
-// ADD A GOOD COMMENT HERE.  Explain exactly what the
-// main function does.
+int *p;
+int n;
+int e;
+int *num_evens;
+// The main function is called at startup and calls all the other functions that run the program.
 int main(int argc, char *argv[]) {
-  // TODO: this is a stub.  Implement me.  
+  p = (int*)malloc(argc*sizeof(int));
+  
+  for(int i = 0; i<argc; i++){
+	p[i] = atoi(argv[i]);
+  }
+
+  n = sizeof(p) / sizeof(p[0]);
+
+ find_evens(p, n, num_evens);
+
+ e = sizeof(num_evens)/sizeof(num_evens[0]);
+
+ print_array(num_evens, e);
+
+ free(p);
+ free(num_evens);
   return 0;
 }
--- hw2/evens/evens_lib.c
+++ hw2/evens/evens_lib.c
@@ -1,11 +1,25 @@
 /* Implementation of the evens library. */
 #include <stdio.h>
+#include <stdlib.h>
 #include "evens_lib.h"
 // add other #includes as needed
 
 int * find_evens(int * p, int n, int * num_evens) {
-  // TODO: this is a stub.  Implement me.
-  return NULL;
+ int j = 0;
+  for(int i=0; i<n; i++){
+	if((p[i])%2 == 0){
+	  j++;
+	}
+  }
+  num_evens = (int*)malloc(j*sizeof(int));
+  j=0;
+  for(int i=0; i<n; i++){
+	if((p[i])%2==0){
+	  num_evens[j]==p[i];
+	  j++;
+	}
+  }
+  return num_evens;
 }
 
 void print_array(int * p, int n) {
