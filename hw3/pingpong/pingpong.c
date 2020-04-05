3456f46994fa91ea7977c54ed6eb25b8a421cc58
Sarah Joyce <sjoyce@ubuntu-1804.udel.edu>
2020-04-05 14:32:47 -0400

    finished pingpong

--- hw3/pingpong/pingpong.c
+++ hw3/pingpong/pingpong.c
@@ -3,40 +3,37 @@
 #include <mpi.h>
 #define NPINGS 1000000
 
-int main(int argc, char *argv[]) {    
-  int rank;
-  int numprocs;
-  int msg1, msg2;
-  double start_time;
-  double finish_time;
-  double total_time;
+int rank;
+int numprocs;
+
+int main(int argc, char *argv[]) {  
+  double start_time, finish_time, total_time;	
   MPI_Init(&argc, &argv);
-  MPI_Status status;
-  //MPI_Barrier(MPI_COMM_WORLD);
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   MPI_Comm_size(MPI_COMM_WORLD, &numprocs);
+  MPI_Barrier(MPI_COMM_WORLD);
 
-    if(rank==0){
-      for(int j = 1; j<numprocs; j++){
+  for(int i=1; i<numprocs; i++){
+    if(rank == 0){
 	start_time = MPI_Wtime();
-  	for (int i=1; i<NPINGS; i++) {
-      	  MPI_Send(&msg1, 0, MPI_INT, j, 99, MPI_COMM_WORLD);
-      	  MPI_Recv(&msg2, 0, MPI_INT, j, 99, MPI_COMM_WORLD, MPI_STATUS_IGNORE);	
+	for(int j=0; j<NPINGS; j++){
+	  MPI_Send(NULL, 0, MPI_CHAR, i, 99, MPI_COMM_WORLD);
+	  MPI_Recv(NULL, 0, MPI_CHAR, i, 99, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 	}
-      finish_time = MPI_Wtime();
-      total_time = (finish_time-start_time)/(2*NPINGS);
-      printf("Average time to trasmit between 0 and %d: %11.10f\n", j, total_time);
-      }
-    }else{
-      for(int j = 1; j<numprocs; j++){
-	for (int i=1; i<NPINGS; i++) {
-	  MPI_Recv(&msg2, 0, MPI_INT, 0, 99, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-	  MPI_Send(&msg1, 0, MPI_INT, 0, 99, MPI_COMM_WORLD);
+    }else if(rank == i){
+	for(int j=0; j<NPINGS; j++){
+	  MPI_Recv(NULL, 0, MPI_CHAR, 0, 99, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+	  MPI_Send(NULL, 0, MPI_CHAR, 0, 99, MPI_COMM_WORLD);
 	}
-      }
     }
-  //MPI_Barrier(MPI_COMM_WORLD);
+    finish_time = MPI_Wtime();
+    total_time = (finish_time-start_time)/(2*NPINGS);
+    MPI_Barrier(MPI_COMM_WORLD);
+     if(rank == 0){
+	printf("Average time to transmit between 0 and %d: %11.10f\n", i, total_time);
+	fflush(stdout);
+     }
+  }
   MPI_Finalize();
-  fflush(stdout);
   return 0;
 }
